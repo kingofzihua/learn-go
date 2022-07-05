8e5abb91da27ae501c0424c2877ec82bbf41747c
kingofzihua <kingofzihua@outlook.com>
2022-07-05 10:59:40 +0800

    feat(ostrich): 修改 app

--- ostrich/app.go
+++ ostrich/app.go
@@ -1,48 +1,38 @@
 package ostrich
 
 import (
-	"context"
-	"sync"
-	"syscall"
-
-	"github.com/Zemanta/gracefulshutdown"
-	"github.com/Zemanta/gracefulshutdown/shutdownmanagers/posixsignal"
+	"flag"
+	"fmt"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+	"os"
+	"runtime"
+	"strings"
 )
 
 type RunFunc func(basename string) error
 
 type App struct {
-	opts    options
-	gs      *gracefulshutdown.GracefulShutdown
-	ctx     context.Context
-	cancel  func()
-	mu      sync.Mutex
-	runFunc RunFunc
-
-	args cobra.PositionalArgs
-	cmd  *cobra.Command
+	basename    string
+	name        string
+	description string
+	runFunc     RunFunc
+	noConfig    bool
+	args        cobra.PositionalArgs
+	cmd         *cobra.Command
 }
 
-func New(opts ...Option) *App {
-	o := options{
-		gsm: posixsignal.NewPosixSignalManager(syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGINT),
-	}
+type Option func(*App)
 
-	for _, opt := range opts {
-		opt(&o)
-	}
-
-	ctx, cancel := context.WithCancel(context.Background())
-	gs := gracefulshutdown.New()
-
-	gs.AddShutdownManager(o.gsm)
-
+func New(name string, basename string, opts ...Option) *App {
 	a := &App{
-		ctx:    ctx,
-		cancel: cancel,
-		opts:   o,
-		gs:     gs,
+		name:     name,
+		basename: basename,
+	}
+
+	for _, o := range opts {
+		o(a)
 	}
 
 	a.buildCommand()
@@ -52,23 +42,72 @@ func New(opts ...Option) *App {
 
 func (a *App) buildCommand() {
 	cmd := cobra.Command{
-		Use:   a.opts.name,
-		Short: a.opts.name,
-		Long:  a.opts.name,
+		Use:   FormatBaseName(a.basename),
+		Short: a.name,
+		Long:  a.description,
 		// stop printing usage when the command errors
 		SilenceUsage:  true,
 		SilenceErrors: true,
 		Args:          a.args,
 	}
+	cmd.SetOut(os.Stdout)
+	cmd.SetErr(os.Stderr)
+	cmd.Flags().SortFlags = true
+
+	cmd.Flags().SetNormalizeFunc(WordSepNormalizeFunc)
+	cmd.Flags().AddGoFlagSet(flag.CommandLine)
+
+	if a.runFunc != nil {
+		cmd.RunE = a.runCommand
+	}
 
 	a.cmd = &cmd
 }
 
-func (a *App) Run() error {
-	// start shutdown managers
-	if err := a.gs.Start(); err != nil {
-		return err
+//
+func (a *App) runCommand(cmd *cobra.Command, args []string) error {
+	// 显示所有的 flags
+	cmd.Flags().VisitAll(func(flag *pflag.Flag) {
+		fmt.Printf("FLAG: --%s=%q \n", flag.Name, flag.Value)
+	})
+
+	if !a.noConfig {
+		if err := viper.BindPFlags(cmd.Flags()); err != nil {
+			return err
+		}
+
+		//if err := viper.Unmarshal(a.options); err != nil {
+		//	return err
+		//}
 	}
 
+	// run application
+	if a.runFunc != nil {
+		return a.runFunc(a.basename)
+	}
+
+	return nil
+}
+
+func (a *App) Run() error {
+
 	return a.cmd.Execute()
+}
+
+// WordSepNormalizeFunc changes all flags that contain "_" separators.
+func WordSepNormalizeFunc(f *pflag.FlagSet, name string) pflag.NormalizedName {
+	if strings.Contains(name, "_") {
+		return pflag.NormalizedName(strings.ReplaceAll(name, "_", "-"))
+	}
+	return pflag.NormalizedName(name)
+}
+
+func FormatBaseName(basename string) string {
+	// Make case-insensitive and strip executable suffix if present
+	if runtime.GOOS == "windows" {
+		basename = strings.ToLower(basename)
+		basename = strings.TrimSuffix(basename, ".exe")
+	}
+
+	return basename
 }
