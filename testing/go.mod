411b563b95f4f3e50ca47889ae98c4e893942a7d
kingofzihua <kingofzihua@outlook.com>
2022-08-11 16:30:10 +0800

    feat(testing): gomock test (#8)
    Signed-off-by: kingofzihua <kingofzihua@outlook.com>

--- testing/go.mod
+++ testing/go.mod
@@ -1,3 +1,5 @@
 module github.com/kingofzihua/learn-go/testing
 
 go 1.18
+
+require github.com/golang/mock v1.6.0
---
+++ testing/gomock/go_version_test.go
@@ -0,0 +1,21 @@
+package gomock
+
+import (
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/kingofzihua/learn-go/testing/gomock/spider"
+)
+
+func TestGetGoVersion(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockSpider := spider.NewMockSpider(ctrl)
+	mockSpider.EXPECT().GetBody().Return("go1.8.3")
+	goVer := GetGoVersion(mockSpider)
+
+	if goVer != "go1.8.3" {
+		t.Errorf("Get wrong version %s", goVer)
+	}
+}
