784735234fca6af7ed22f4d1caf723da440f8ca3
kingofzihua <kingofzihua@outlook.com>
2023-01-31 14:58:18 +0800

    feat(web/gin): init router
    Signed-off-by: kingofzihua <kingofzihua@outlook.com>

---
+++ web/gin/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+// /h el lo /gin
+// /h el lo 2/gin
+// /h el o :username /name
+// /h i:usernamess
+
+func main() {
+	router := gin.New()
+
+	router.GET("/index", Index)
+	router.GET("/hello/gin", HelloGin)
+	router.GET("/hello2/gin", HelloGin)
+	/**
+
+	/user/gordon              match
+	/user/you                 match
+	/user/gordon/profile      no match
+	/user/                    no match
+
+	*/
+	router.GET("/user/:username", User)
+	router.GET("/hi:username", User)        // 允许
+	router.GET("/helo:username/Name", User) // 允许
+	//router.GET("/:d-:m-:y/name", User)    // 不允许
+
+	//router.GET("/blog/:/", BlogIndex) // params 路由必须有名字
+	// router.GET("/blog/index", BlogIndex) // 同一个路由前缀，不允许同时定义 static 和 params
+
+	/**
+	/blog/go/request-routers            match: category="go", post="request-routers"
+	/blog/go/request-routers/           no match, but the router would redirect
+	/blog/go/                           no match
+	/blog/go/request-routers/comments   no match
+	*/
+	router.GET("/blog/:category/:post/", Blog)
+	router.GET("/blog/:category/:post/:name", Blog)
+	router.GET("/bg/:category/:post/:name", Blog)
+
+	/**
+	/static/                     match
+	/static/index.html           match
+	/static/js/test.js           match
+	*/
+	router.StaticFS("/static/", http.Dir("./public"))
+
+	log.Fatal(router.Run(":8080"))
+}
+
+func Index(c *gin.Context) {
+	c.String(http.StatusOK, "Welcome!\n")
+}
+
+func HelloGin(c *gin.Context) {
+	c.String(http.StatusOK, "hello gin")
+}
+
+func BlogIndex(c *gin.Context) {
+	c.String(http.StatusOK, "blog index")
+}
+
+func Blog(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"category": c.Param("category"), "post": c.Param("post")})
+}
+
+func User(c *gin.Context) {
+	c.String(http.StatusOK, "hello, %s!\n", c.Param("username"))
+}
