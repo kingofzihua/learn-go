727b2b0752315f0445a3d4c3c1361a873d5bbc99
kingofzihua <kingofzihua@outlook.com>
2023-07-27 14:29:19 +0800

    feat(log): kratos log
    Signed-off-by: kingofzihua <kingofzihua@outlook.com>

--- logger/go.mod
+++ logger/go.mod
@@ -3,3 +3,5 @@ module github.com/kingofzihua/learn-go/logger
 go 1.20
 
 require golang.org/x/exp v0.0.0-20230725012225-302865e7556b
+
+require github.com/go-kratos/kratos/v2 v2.6.3
--- logger/main.go
+++ logger/main.go
@@ -2,23 +2,28 @@ package main
 
 import (
 	"context"
-	"github.com/kingofzihua/learn-go/logger/kratos"
+	"github.com/go-kratos/kratos/v2/log"
 	"golang.org/x/exp/slog"
+	"io"
+	"net"
 	"os"
 )
 
 func main() {
-	//slogs()
-	kraots()
+	file, _ := os.OpenFile("test.log", os.O_WRONLY|os.O_CREATE|os.O_TRUNC|os.O_TRUNC, os.ModePerm)
+
+	slogs(file)
+	kraots(file)
 }
 
-func kraots() {
-	logger := kratos.NewStdLogger(os.Stdout)
+func kraots(w io.Writer) {
 
-	logger = kratos.With(logger,
-		"time", kratos.DefaultTimestamp,
-		"caller", kratos.DefaultCaller,
-		"user", kratos.Valuer(func(ctx context.Context) any {
+	logger := log.NewStdLogger(w)
+
+	logger = log.With(logger,
+		"time", log.DefaultTimestamp,
+		"caller", log.DefaultCaller,
+		"user", log.Valuer(func(ctx context.Context) any {
 			u, ok := ctx.Value("name").(string)
 			if ok {
 				return u
@@ -28,21 +33,31 @@ func kraots() {
 	)
 
 	ctx := context.WithValue(context.Background(), "name", "kingofzihua")
-	logger = kratos.WithContext(ctx, logger)
+	logger = log.WithContext(ctx, logger)
 
-	SetDefault(logger)
+	log.SetLogger(logger)
 
-	Infof("hello %s", "world")
+	log.Infof("hello %s", "world")
+	log.Context(ctx).Infow("msg", "warn msg", "domain", "baidu")
+	log.Error("oops", "err", net.ErrClosed, "status", 500)
 }
 
-func slogs() {
-	l := slog.New(slog.NewJSONHandler(os.Stderr, nil))
-
-	slog.SetDefault(l.With("name", func(ctx context.Context) string {
-		return ctx.Value("name").(string)
+func slogs(w io.Writer) {
+	l := slog.New(slog.NewTextHandler(w, &slog.HandlerOptions{
+		AddSource: true,
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			// Remove time from the output for predictable test output.
+			if a.Key == "name" {
+				return slog.Attr{}
+			}
+			return a
+		},
 	}))
+
+	slog.SetDefault(l.With("name", "default"))
 
 	ctx := context.WithValue(context.Background(), "name", "kingofzihua")
 
-	slog.InfoContext(ctx, "warn msg")
+	slog.InfoContext(ctx, "warn msg", "domain", "baidu")
+	slog.Error("oops", "err", net.ErrClosed, "status", 500)
 }
